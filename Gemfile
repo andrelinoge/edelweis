0d2bc445f3e4529f654693957ad1910f2812cf90
Andre Linoge <andrelinoge87@gmail.com>
2017-01-31 12:18:27 -0800

    added welcome page

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'bcrypt', '~> 3.1.7'
 gem 'bootstrap-sass', '~> 3.3.1'
 gem 'coffee-rails', '~> 4.2'
 gem 'devise'
+gem 'font-awesome-rails'
 # G H I J K L 
 gem 'jbuilder', '~> 2.5'
 gem 'jquery-rails'
@@ -20,6 +21,7 @@ gem 'rails', '~> 5.0.1'
 gem 'redis', '~> 3.0'
 # S T U V W X Y Z
 gem 'sass-rails', '~> 5.0'
+gem 'slim-rails'
 gem 'turbolinks', '~> 5'
 gem 'uglifier', '>= 1.3.0'
 
---
+++ app/assets/javascripts/admin-manifest.js
@@ -0,0 +1,34 @@
+//= require jquery
+//= require jquery_ujs
+
+//= require bootstrap
+//= require js/flot/jquery.flot.js
+//= require js/flot/jquery.flot.pie.js
+//= require js/flot/jquery.flot.orderBars.js
+//= require js/flot/jquery.flot.time.min.js
+//= require js/flot/date.js
+//= require js/flot/jquery.flot.spline.js
+//= require js/flot/jquery.flot.stack.js
+//= require js/flot/curvedLines.js
+//= require js/flot/jquery.flot.resize.js
+
+//= require js/progressbar/bootstrap-progressbar.min.js
+
+//= require js/icheck/icheck.min.js
+
+//= require js/moment/moment.min.js
+//= require bootstrap-datepicker
+
+//= require js/chartjs/chart.min.js
+
+//= require js/pace/pace.min.js
+
+//= require js/datatables/jquery.dataTables.min.js
+//= require js/datatables/dataTables.responsive.min.js
+//= require js/datatables/responsive.bootstrap.min.js
+
+//= require ckeditor/init
+//= require jquery.Jcrop
+//= require jcrop_on_fly.js
+
+//= require_tree ./admin
