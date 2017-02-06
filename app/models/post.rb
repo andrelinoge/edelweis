274c6ad5fa8dd0f66675c523551a535a6df77083
Andre Linoge <andrelinoge87@gmail.com>
2017-02-06 03:51:16 -0800

    added rus localization

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,9 @@
 
 class Post < ApplicationRecord
   translates :title, :content, :meta_keys, :meta_description
+
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :meta_keys, presence: true
+  validates :meta_description, presence: true
 end
