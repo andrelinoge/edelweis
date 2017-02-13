c3c9a3b7391ff093ee0845d1e779e6081d8a8944
Andre Linoge <andrelinoge87@gmail.com>
2017-02-13 13:19:53 -0800

    started main page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,22 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap_sb_admin_base_v2
+//= require unslider
+
+$(function() {
+  $('#lang-switcher').mouseenter(function() {
+    $('#lang-options').show();
+  })
+  .mouseleave(function() {
+    $('#lang-options').hide();
+  });
+
+  $('#slider').unslider({
+    animation: 'fade',
+    autoplay: true,
+    arrows: false,
+    nav: false
+  });
+
+});
+
